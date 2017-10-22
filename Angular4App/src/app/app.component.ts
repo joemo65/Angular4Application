e4fbccf3a16851131c7aa1c3554576b52427b32a
Joseph Moore <joemo65@gmail.com>
2017-10-22 11:46:50 -0700

    added bootstrap

--- Angular4App/src/app/app.component.ts
+++ Angular4App/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.tests.html', //TODO: revert back and figure out how to add multiple components 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = '';
 }
