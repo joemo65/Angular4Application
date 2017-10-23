ce6e604560bfdea07615eb13412fd292902a7b96
Joseph Moore <joemo65@gmail.com>
2017-10-22 22:29:45 -0700

    moved header into own component

--- Angular4App/src/app/app.module.ts
+++ Angular4App/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { Example1Component } from './example1/example1.component';
+import { LinksComponent } from './links/links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Example1Component
+    HeaderComponent,
+    Example1Component,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
