62ceae83e15fd88c13ea731f356129ebcc9e0531
Joseph Moore <joemo65@gmail.com>
2017-10-22 20:40:21 -0700

    added example1 component

--- Angular4App/src/app/app.component.ts
+++ Angular4App/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html', //TODO: revert back and figure out how to add multiple components 
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = '';
 }
--- Angular4App/src/app/app.module.ts
+++ Angular4App/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Example1Component } from './example1/example1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Example1Component
   ],
   imports: [
     BrowserModule,
