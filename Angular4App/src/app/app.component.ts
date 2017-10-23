e7bc8d26d531f81d1492fda28d802961dc9f48d4
Joseph Moore <joemo65@gmail.com>
2017-10-22 18:52:25 -0700

    added example component and links components

--- Angular4App/src/app/app.component.ts
+++ Angular4App/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.tests.html', //TODO: revert back and figure out how to add multiple components 
+  templateUrl: './app.component.html', //TODO: revert back and figure out how to add multiple components 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '';
 }
